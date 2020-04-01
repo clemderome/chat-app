656352e59a1c478a29529ccb1c5e02989e69e2a7
Clementine DEROME <clementine.derome@gmail.com>
2020-04-01 12:22:39 +0200

    React 02.1.2

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -11,11 +11,15 @@ const status = user.status ? 'online' : 'offline';
 const Contact = () => {
   return (
     <div className="Contact">
-      <img className="avatar" src={user.avatar}/>
+      <div>
+        <img className="avatar" src={user.avatar}/>
+      </div>
       <div className="status"> 
-        <h4 className="name" src={user.name}>{user.name}</h4>
-        <p className="status-online" src={status}> {user.status ? ' ' : ' '}</p>
-        <p className="status, status-text" src={status}> {user.status ? 'Online' : 'Offline'}</p>
+          <h4 className="name" src={user.name}>{user.name}</h4>
+        <div className="status">
+          <div className="status" src={status}>
+            {user.status ? <span className="status-online"></span> : <span className="status-offline"></span>} {user.status ? 'Online' : 'Offline'}</div>
+          </div>
       </div>
     </div>
   )
